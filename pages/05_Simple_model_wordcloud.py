d601e22052f3cd1dffdfe27f4e2067c4819d2525
Declan Stockdale Garbutt <53500810+Declan-Stockdale@users.noreply.github.com>
2022-10-14 16:00:46 +1100

    Add files via upload

---
+++ pages/05_Simple_model_wordcloud.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import pandas as pd
+from top2vec import Top2Vec
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+st.header('Generate Word clouds for Top2Vec model')
+
+# Check that mdoel has been loaded in
+if 'top2vec_model' not in st.session_state or st.session_state.top2vec_model == False:
+
+
+    st.write('Please run Top2Vec model first before looking at this page')
+
+else:
+    # model exists
+    top2vec_model = st.session_state.top2vec_model
+
+    # click to generate ouytput
+    st.session_state['show_wordcloud_button'] = True
+    top2vec_topic_nums = st.session_state['top2vec_topic_nums']
+
+    # Show how many topics found
+    st.write(f"Number of topics found = {top2vec_topic_nums[-1]+1}")
+    if "show_wordcloud_button" not in st.session_state:
+        st.session_state['show_wordcloud_button'] = False
+
+    st.write('Images can be downloaded by right clicking and saving as png')
+    # click to create wordlcouds
+    show_wordcloud_button = st.button('Show word cloud for each topic')
+    if show_wordcloud_button:
+
+        # loop over topics
+        for topic in top2vec_topic_nums:
+            # use inbuilt model function to generate wordclouds
+            fig = top2vec_model.generate_topic_wordcloud(topic, background_color="black")
+            st.pyplot(fig)
---
+++ pages/12_Work_in_progress.py
@@ -0,0 +1,60 @@
+from __future__ import print_function
+import streamlit as st
+from streamlit import components
+import pyLDAvis
+import pyLDAvis.sklearn
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.decomposition import LatentDirichletAllocation
+
+st.header('PyLDAVis - not functional yet')
+st.write('There is an error with displaying the output which is ye')
+#st.write('Using all-MiniLM-L6-v2 as the embedding model')
+
+
+if "df_working" not in st.session_state:
+    st.write('Please upload a json file in previous page before proceeding with Berttopic modelling')
+else:
+    df_working = st.session_state['df_working']
+
+    proceed_button_pyldavis =  st.button('Proceed with PyLDAVis')
+
+    if proceed_button_pyldavis:
+
+        tf_vectorizer = CountVectorizer(strip_accents = 'unicode',
+                    stop_words = 'english',
+                    lowercase = True,
+                    token_pattern = r'\b[a-zA-Z]{3,}\b',
+                    max_df = 0.5,
+                    min_df = 10)
+
+
+        tfidf_vectorizer = TfidfVectorizer(**tf_vectorizer.get_params())
+        dtm_tfidf = tfidf_vectorizer.fit_transform(df_working.Data.values.tolist())
+
+
+        lda_tfidf = LatentDirichletAllocation(n_components=20, random_state=0)
+        lda_tfidf.fit(dtm_tfidf)
+
+        st.write(' Visualisation of PyLDAVis')
+
+        pyldavis_vis = pyLDAvis.sklearn.prepare(lda_tfidf, dtm_tfidf, tfidf_vectorizer)
+        pyLDAvis.save_html(pyldavis_vis, 'lda.html')
+        st.write('pyldavis_vis')
+        st.write(pyldavis_vis)
+        #pyLDAvis.save_html(pyldavis_vis, 'lda.html')
+
+
+        #from streamlit import components
+        #with open('./lda.html', 'r') as f:
+            #html_string = f.read()
+
+        #components.v1.html(html_string, width=1300, height=800, scrolling=False)
+
+        st.write(pyldavis_vis)
+
+        with open('./lda.html', 'r') as f:
+            html_string = f.read()
+        components.v1.html(html_string, width=1300, height=800, scrolling=False)
+
+        st.write('above as plotly chart')
+        st.html(pyldavis_vis)
